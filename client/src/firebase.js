3418019d1d4a997bd63f3e091340477a3120964d
omgour113 <omgour113@gmail.com>
2023-12-07 11:01:14 +0530

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDND37NQNVMwmEq7sWy_NIsTYx0G2MAzNI",
+  authDomain: "mern-realtor-9ac75.firebaseapp.com",
+  projectId: "mern-realtor-9ac75",
+  storageBucket: "mern-realtor-9ac75.appspot.com",
+  messagingSenderId: "665524133461",
+  appId: "1:665524133461:web:b5a63d5d34d548859ebedc"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
